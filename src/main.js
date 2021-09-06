fca41c427128aec448e43c728eea5872013a3614
litianhua <18702971504@163.com>
2021-09-06 17:27:27 +0800

    ✨ 特性: 引入store

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 // 引入element-plus插件
 import ElemnentPlusPlugin from './plugins/element-plugin.js'
 
 const app = createApp(App)
 
-app.use(ElemnentPlusPlugin).use(router).mount('#app')
+app.use(ElemnentPlusPlugin).use(store).use(router).mount('#app')
