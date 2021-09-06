91e6c85883d495f464131b6641d1340922e1b1eb
litianhua <18702971504@163.com>
2021-09-06 15:39:39 +0800

    ✨ 特性: 引入header

---
+++ src/components/header.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-header class="header">
+    <el-row :gutter="8">
+      <el-col :span="1">
+        <el-icon class="collapse-btn" color="#409EFC">
+          <fold />
+        </el-icon>
+      </el-col>
+    </el-row>
+
+  </el-header>
+</template>
+
+<script>
+  import { Fold } from '@element-plus/icons'
+
+  export default {
+    components: {
+      Fold
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .header {
+    position: relative;
+    box-sizing: border-box;
+    width: 100%;
+    height: 70px;
+    font-size: 22px;
+    color: #fff;
+    .collapse-btn {
+      padding: 0 21px;
+      cursor: pointer;
+      line-height: 70px;
+      height: 70px;
+      font-size: 22px;
+    }
+  }
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,15 +1,21 @@
 <template>
-  <div id="home">ddd</div>
+  <div id="home">
+    <page-header />
+  </div>
 </template>
 
 <script>
-export default {
-  
-}
+  import PageHeader from '@/components/header.vue'
+
+  export default {
+    components: {
+      PageHeader
+    }
+  }
 </script>
 
 <style lang="less" scoped>
-#home {
-  color: red;
-}
+  #home {
+    // color: red;
+  }
 </style>
