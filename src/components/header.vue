445459bf2a3b887118d7034ec821d5fed616ffc7
litianhua <18702971504@163.com>
2021-09-06 17:03:13 +0800

    ✨ 特性: 增加下拉菜单

--- src/components/header.vue
+++ src/components/header.vue
@@ -10,7 +10,10 @@
       <el-col class="logo" :span="2">后台管理系统</el-col>
       <el-col :span="4" :offset="17">
         <el-row justify="center" align="middle" style="height: 70px;" :gutter="8">
-          <el-col class="btn-cell" :span="2">
+          <!-- 占位 -->
+          <el-col :span="4"></el-col>
+          <!-- 消息通知 -->
+          <el-col class="btn-cell" :span="4">
             <el-tooltip content="消息中心">
               <router-link to="/tabs">
                 <i class="el-icon-bell"></i>
@@ -18,11 +21,26 @@
             </el-tooltip>
             <span class="btn-bell-badge"></span>
           </el-col>
-          <el-col class="user-avator" style="text-align: center;" :span="4">
+          <!-- 头像 -->
+          <el-col class="user-avator" style="text-align: center;" :span="8">
             <img src="../assets/img/img.jpg" alt="用户头像">
           </el-col>
-          <el-col style="text-align: center;" :span="18">
-            用户名下拉菜单
+          <!-- 下拉菜单 -->
+          <el-col class="dropdown-menu" style="text-align: center;" :span="8">
+            <el-dropdown trigger="click">
+              <span class="el-dropdown-link">
+                下拉菜单<i class="el-icon-caret-bottom el-icon--right"></i>
+              </span>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <a href="https://github.com/lin-xin/vue-manage-system" target="_blank">
+                    <el-dropdown-item>仿照的项目仓库</el-dropdown-item>
+                  </a>
+                  <el-dropdown-item >个人中心</el-dropdown-item>
+                  <el-dropdown-item divided>退出登录</el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
           </el-col>
         </el-row>
       </el-col>
@@ -57,7 +75,8 @@
       line-height: 70px;
     }
     .user-avator,
-    .btn-cell {
+    .btn-cell,
+    .dropdown-menu {
       text-align: center;
       cursor: pointer;
     }
