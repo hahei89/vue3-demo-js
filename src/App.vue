d5bfaeab4393962a3e3b2993f293f99cf9e98740
litianhua <18702971504@163.com>
2021-09-06 14:21:50 +0800

    💄 界面: 添加样式文件

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
-<script setup>
-
-</script>
-
 <template>
   <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<script setup>
+
+</script>
+
+<style lang="less">
+@import './assets/css/main.css';
+@import './assets/css/color-dark.css';
+
 </style>
