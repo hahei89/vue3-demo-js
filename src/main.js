3debc780fd0dd4dd44daac5d4967ce867d012cf6
litianhua <18702971504@163.com>
2021-09-06 14:17:18 +0800

    引入element-plus插件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 引入element-plus插件
+import ElemnentPlusPlugin from './plugins/element-plugin.js'
+
 const app = createApp(App)
 
-app.use(router).mount('#app')
+app.use(ElemnentPlusPlugin).use(router).mount('#app')
