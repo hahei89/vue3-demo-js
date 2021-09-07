4734d632dd187c6d1dc757c9b65bbf5487219d84
litianhua <18702971504@163.com>
2021-09-07 14:45:46 +0800

    ✨ 特性: 调整侧边栏宽度

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,30 +1,43 @@
 <template>
   <el-container>
-    <el-header class="header"> 
+    <el-header class="header">
       <page-header />
     </el-header>
     <el-container>
-      <el-aside width="200px">
+      <el-aside :width="collapse ? 'auto' : '200px'">
         <page-side-bar />
       </el-aside>
-      <el-main>Main</el-main>
+      <el-main>
+        Main
+      </el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
+  import { computed } from 'vue'
+  import { useStore } from 'vuex'
   import PageHeader from '@/components/header.vue'
   import PageSideBar from '@/components/side-bar.vue'
+
   export default {
     components: {
       PageHeader,
       PageSideBar
+    },
+    setup (props) {
+      const store = useStore()
+
+      const collapse = computed(() => store.state.collapse)
+
+      return {
+        collapse
+      }
     }
   }
 </script>
 
 <style lang="less" scoped>
   .el-aside {
-   
   }
 </style>
