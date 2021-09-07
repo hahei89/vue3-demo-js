1ccca91fa5af94d315e9d8c89bf353cdb090d205
litianhua <18702971504@163.com>
2021-09-07 15:07:01 +0800

    ✨ 特性: 使用标签展示路由

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -8,7 +8,7 @@
         <page-side-bar />
       </el-aside>
       <el-main>
-        Main
+        <page-tags :tags="tags" />
       </el-main>
     </el-container>
   </el-container>
@@ -19,18 +19,28 @@
   import { useStore } from 'vuex'
   import PageHeader from '@/components/header.vue'
   import PageSideBar from '@/components/side-bar.vue'
-
+  import PageTags from '@/components/tags.vue'
   export default {
     components: {
       PageHeader,
-      PageSideBar
+      PageSideBar,
+      PageTags
     },
     setup (props) {
+      const tags = [
+        { type: '', label: '标签一' },
+        { type: 'success', label: '标签二' },
+        { type: 'info', label: '标签三' },
+        { type: 'danger', label: '标签四' },
+        { type: 'warning', label: '标签五' },
+      ]
+
       const store = useStore()
 
       const collapse = computed(() => store.state.collapse)
 
       return {
+        tags,
         collapse
       }
     }
