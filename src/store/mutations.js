60e96718dfaa2141539eb13bfabcd728e9487f96
litianhua <18702971504@163.com>
2021-09-07 17:20:33 +0800

    ✨ 特性: store增加taglist

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,6 +2,24 @@ function handleCollapse(state, data) {
   state.collapse = data
 }
 
+function addTag(state, tag) {
+  // 已存在，则不做任何操作
+  if (state.tagsList.findIndex((item) => item.path === tag.path) > -1) {
+    return
+  }
+  state.tagsList.push(tag)
+}
+
+function removeTag(state, tag) {
+  // 已存在且不是首页
+  const tagIndex = state.tagsList.findIndex((item) => item.path === tag.path)
+  if (tagIndex > -1 && tag.path !== '/') {
+    state.tagsList.splice(tagIndex, 1)
+  }
+}
+
 export default {
-  handleCollapse
+  handleCollapse,
+  addTag,
+  removeTag
 }
--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,12 @@
 const state = {
-  collapse: false
+  collapse: false,
+  tagsList: [
+    {
+      title: '首页',
+      path: '/',
+      name: 'Home'
+    }
+  ]
 }
 
 export default state
