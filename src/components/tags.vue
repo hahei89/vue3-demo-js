e784d7ca40e299e38a64303afe29eb88d3915bf0
litianhua <18702971504@163.com>
2021-09-07 15:06:49 +0800

    ✨ 特性: 添加标签页

---
+++ src/components/tags.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="tag-group">
+    <el-tag
+      v-for="item in tags"
+      :key="item.label"
+      :type="item.type"
+      effect="dark">
+      {{ item.label }}
+    </el-tag>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['tags'],
+    setup () {
+      
+      return {
+      }
+    }
+  }
+</script>
+
+<style lang="less" scope>
+</style>
