14331b076e7314cedd47efd2f68522b5e1724081
litianhua <18702971504@163.com>
2021-09-08 10:42:09 +0800

    🔧 配置: 增加路径别名

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,9 @@ export default defineConfig({
   resolve: {
     alias: {
       '@/': `${path.resolve(__dirname, 'src')}/`,
-      '@': `${path.resolve(__dirname, 'src')}`
+      '@': `${path.resolve(__dirname, 'src')}`,
+      '@components/': `${path.resolve(__dirname, 'src/components')}/`,
+      '@components': `${path.resolve(__dirname, 'src/components')}`
     }
   }
 })
