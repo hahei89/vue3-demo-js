3d2ddc7fd9f841ca71680973014feabc89ad5c1c
litianhua <18702971504@163.com>
2021-09-08 10:54:52 +0800

    ✨ 特性: 增加路由展示视图

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -8,7 +8,17 @@
         <page-side-bar />
       </el-aside>
       <el-main>
+        <!-- 标签页 -->
         <page-tags />
+        <div class="content">
+          <router-view v-slot="{Component}">
+            <transition name="move" mode="out-in">
+              <keep-alive>
+                <component :is="Component" />
+              </keep-alive>
+            </transition>
+          </router-view>
+        </div>
       </el-main>
     </el-container>
   </el-container>
@@ -17,9 +27,9 @@
 <script>
   import { computed } from 'vue'
   import { useStore } from 'vuex'
-  import PageHeader from '@/components/header.vue'
-  import PageSideBar from '@/components/side-bar.vue'
-  import PageTags from '@/components/tags.vue'
+  import PageHeader from '@components/header.vue'
+  import PageSideBar from '@components/side-bar.vue'
+  import PageTags from '@components/tags.vue'
   export default {
     components: {
       PageHeader,
@@ -29,10 +39,15 @@
     setup (props) {
       const store = useStore()
 
+      const tagsListName = computed(() =>
+        store.state.tagsList.map((tag) =>
+          tag.name)
+      )
       const collapse = computed(() => store.state.collapse)
 
       return {
-        collapse
+        collapse,
+        tagsListName
       }
     }
   }
