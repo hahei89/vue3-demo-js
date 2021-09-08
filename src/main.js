8e2efd94084718ccb42385899cefe667a3e0ef57
litianhua <18702971504@163.com>
2021-09-08 14:44:06 +0800

    ✨ 特性: 配置403页面

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/css/icon.css'
 
 // 引入element-plus插件
 import ElemnentPlusPlugin from './plugins/element-plugin.js'
--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,7 @@ const routes = [
         meta: {
           title: '系统首页'
         },
-        component: () => import ( /* webpackChunkName: "tabs" */ "../pages/dashboard/index.vue")
+        component: () => import ( /* webpackChunkName: "dashboard" */ "../pages/dashboard/index.vue")
       },
       {
         path: '/tabs',
@@ -26,6 +26,15 @@ const routes = [
           title: '标签页'
         },
         component: () => import ( /* webpackChunkName: "tabs" */ "../pages/tabs/index.vue")
+      },
+      {
+        path: '/permission',
+        name: 'permission',
+        meta: {
+          title: '权限管理',
+          permission: true
+        },
+        component: () => import ( /* webpackChunkName: "permission" */ "../pages/permission/index.vue")
       }
     ]
   }
@@ -34,6 +43,18 @@ const routes = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title | 'vue-manage-system'}`
+  const role = localStorage.getItem('ms_username')
+  if (!role && to.path !== '/login') {
+    next('/login')
+  } else if (to.meta.permission) {
+    role === 'admin' ? next() : next('/403')
+  } else {
+    next()
+  }
 })
 
 export default router
